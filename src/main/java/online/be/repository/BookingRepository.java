88061d66c9ee2dd27acb6aaaab4a8642b1392ca7
Thinh Nguyen <thinhnd20032007@gmail.com>
2024-06-08 15:04:28 +0700

    Fix 105 errors

--- src/main/java/online/be/repository/BookingRepository.java
+++ src/main/java/online/be/repository/BookingRepository.java
@@ -1,6 +1,8 @@
 package online.be.repository;
 
+import online.be.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
