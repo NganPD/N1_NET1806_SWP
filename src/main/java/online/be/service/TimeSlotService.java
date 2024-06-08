d031244cf06a55d35cf40e5d4b2a156ef4c845f1
ngan <nganpdse171513@fpt.edu.vn>
2024-06-08 12:49:04 +0700

    Reference TimeSlot

--- src/main/java/online/be/service/TimeSlotService.java
+++ src/main/java/online/be/service/TimeSlotService.java
@@ -1,17 +1,13 @@
 package online.be.service;
 
-import online.be.entity.Court;
 import online.be.entity.CourtSchedule;
 import online.be.entity.TimeSlot;
-import online.be.entity.Venue;
-import online.be.model.Request.CourtRequest;
 import online.be.model.Request.TimeSlotRequest;
 import online.be.repository.CourtScheduleRepository;
 import online.be.repository.TimeSlotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Time;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
@@ -22,41 +18,43 @@ public class TimeSlotService {
     @Autowired
     private TimeSlotRepository timeSlotRepository;
 
-    private CourtScheduleRepository courtScheduleRepository;
+    private final CourtScheduleRepository courtScheduleRepository;
+
+    public TimeSlotService(TimeSlotRepository timeSlotRepository, CourtScheduleRepository courtScheduleRepository) {
+        this.timeSlotRepository = timeSlotRepository;
+        this.courtScheduleRepository = courtScheduleRepository;
+    }
 
     // Lưu một TimeSlot mới hoặc cập nhật một TimeSlot đã tồn tại
-    public TimeSlot createTimeSlot(TimeSlotRequest timeSlotRequest){
+    public TimeSlot createTimeSlot(TimeSlotRequest timeSlotRequest) {
         TimeSlot timeSlot = new TimeSlot();
         CourtSchedule courtSchedule = courtScheduleRepository.findById(timeSlotRequest.getCourtScheduleId()).get();
-        if(courtSchedule != null){
-            timeSlot.setDuration(timeSlotRequest.getDuration());
-            timeSlot.setStartTime(timeSlotRequest.getStartTime());
-            timeSlot.setEndTime(timeSlotRequest.getEndTime());
-            timeSlot.setCourtSchedule(courtSchedule);
-            return timeSlotRepository.save(timeSlot);
-        } else {
+        if (courtSchedule == null) {
             throw new RuntimeException("CourtScheduleId is not existed: " + timeSlotRequest.getCourtScheduleId());
         }
+        timeSlot.setDuration(timeSlotRequest.getDuration());
+        timeSlot.setStartTime(timeSlotRequest.getStartTime());
+        timeSlot.setEndTime(timeSlotRequest.getEndTime());
+        timeSlot.setCourtSchedule(courtSchedule);
+        return timeSlotRepository.save(timeSlot);
     }
 
     // Cập nhật thông tin TimeSlot
-    public TimeSlot updateTimeSlot(long timeSlotId, TimeSlotRequest timeSlotRequest){
-        Optional<TimeSlot>timeSlotOptional = timeSlotRepository.findById(timeSlotId);
-        if(timeSlotOptional.isPresent()){
-            TimeSlot timeSlot = new TimeSlot();
-            CourtSchedule courtSchedule = courtScheduleRepository.findById(timeSlotRequest.getCourtScheduleId()).get();
-            if(courtSchedule != null){
-                timeSlot.setDuration(timeSlotRequest.getDuration());
-                timeSlot.setStartTime(timeSlotRequest.getStartTime());
-                timeSlot.setEndTime(timeSlotRequest.getEndTime());
-                timeSlot.setCourtSchedule(courtSchedule);
-                return timeSlotRepository.save(timeSlot);
-            } else {
-                throw new RuntimeException("CourtScheduleId is not existed: " + timeSlotRequest.getCourtScheduleId());
-            }
-        }else{
+    public TimeSlot updateTimeSlot(long timeSlotId, TimeSlotRequest timeSlotRequest) {
+        Optional<TimeSlot> timeSlotOptional = timeSlotRepository.findById(timeSlotId);
+        if (timeSlotOptional.isEmpty()) {
             throw new RuntimeException("TimeSlot not found with ID: " + timeSlotId);
         }
+        TimeSlot timeSlot = new TimeSlot();
+        CourtSchedule courtSchedule = courtScheduleRepository.findById(timeSlotRequest.getCourtScheduleId()).get();
+        if (courtSchedule == null) {
+            throw new RuntimeException("CourtScheduleId is not existed: " + timeSlotRequest.getCourtScheduleId());
+        }
+        timeSlot.setDuration(timeSlotRequest.getDuration());
+        timeSlot.setStartTime(timeSlotRequest.getStartTime());
+        timeSlot.setEndTime(timeSlotRequest.getEndTime());
+        timeSlot.setCourtSchedule(courtSchedule);
+        return timeSlotRepository.save(timeSlot);
     }
 
     // Tìm các TimeSlot theo độ dài
@@ -70,22 +68,22 @@ public class TimeSlotService {
     }
 
     // Hiển thị TimeSlot dựa trên id
-    public TimeSlot getTimeSlotById(long timeSlotId){
+    public TimeSlot getTimeSlotById(long timeSlotId) {
         Optional<TimeSlot> timeSlotOptional = timeSlotRepository.findById(timeSlotId);
-        if(timeSlotOptional.isPresent()){
+        if (timeSlotOptional.isPresent()) {
             return timeSlotOptional.get();
-        }else{
+        } else {
             throw new RuntimeException("TimeSlot not found with ID: " + timeSlotId);
         }
     }
 
     // Hiển thị toàn bộ TimeSlot
-    public List<TimeSlot> getAllTimeSlots(){
+    public List<TimeSlot> getAllTimeSlots() {
         return timeSlotRepository.findAll();
     }
 
     // Xoá một TimeSlot
-    public void deleteTimeSlot(Long timeSlotId){
+    public void deleteTimeSlot(Long timeSlotId) {
         timeSlotRepository.deleteById(timeSlotId);
     }
 }
