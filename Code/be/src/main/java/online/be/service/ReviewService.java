b022b2c74936e44220af1ab4c088147870abc160
PhamDinhNgan <169060597+NganPD@users.noreply.github.com>
2024-07-31 15:52:22 +0700

    Update ReviewService.java

--- Code/be/src/main/java/online/be/service/ReviewService.java
+++ Code/be/src/main/java/online/be/service/ReviewService.java
@@ -9,6 +9,8 @@ import online.be.model.Request.ReviewRequest;
 import online.be.repository.ReviewRepository;
 import online.be.repository.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -57,8 +59,9 @@ public class ReviewService {
         return reviewRepository.findAll();
     }
 
-    public List<Venue> getTopRatedVenues(){
-        return reviewRepository.findTopRatedVenues();
+    public List<Venue> getTopRatedVenues() {
+        Pageable topFive = PageRequest.of(0, 5);
+        return reviewRepository.findTopRatedVenues(topFive);
     }
 
 }
